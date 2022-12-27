673d3a19b92c395829943ea3fd0c74b78614b616
Diogo Veloso <diogo.costa.veloso@gmail.com>
2022-12-27 21:41:11 +0000

    Update
    -Deff stage update

---
+++ Assets/Generate/Stage_Deff/DeffStageController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeffStageController : MonoBehaviour
+{
+    [SerializeField] PlateScript plate;
+    TextMeshProUGUI txtPlate;
+    void Start()
+    {
+        txtPlate = GameObject.Find("DeffText").GetComponent<TextMeshProUGUI>();
+        txtPlate.enabled = false;
+    }
+
+    void Update()
+    {
+        UpdateUI();
+    }
+    void UpdateUI()
+    {
+        float value = Mathf.Round(plate.PlatePorc*100f);
+        txtPlate.text = "Capture the Plate\n Plate: "+value.ToString()+"%";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            txtPlate.enabled = true;
+
+        }
+    }
+}
