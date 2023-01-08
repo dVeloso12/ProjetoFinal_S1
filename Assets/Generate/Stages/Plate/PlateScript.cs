efe6b41c79f5ae250bdcf944234408a294f4600a
Ze-Alves <josepalves2511@gmail.com>
2023-01-08 23:11:21 +0000

    z

--- Assets/Generate/Stages/Plate/PlateScript.cs
+++ Assets/Generate/Stages/Plate/PlateScript.cs
@@ -55,6 +55,7 @@ public class PlateScript : MonoBehaviour
         if(PlatePorc >= 1f)
         {
             PlateCompleted = true;
+            spwaner.activated = false;
             //PlatePorc = 0f;
         }
         if(PlateCompleted)
